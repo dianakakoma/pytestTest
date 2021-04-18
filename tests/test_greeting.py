9b7e907bf5d4b2942763d613359172d4551bebd9
dianakakoma <dikakoma@gmail.com>
2021-04-18 13:13:43 -0500

    corrected a typo with the greet variable declation.

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -1,2 +1,8 @@
 import pytest
 from lib.greeting import greeting
+
+class TestGreeting:
+    
+    def test_greeting(self):
+        greet = greeting("Charlie")
+        assert greet == "Hello, Charlie."
